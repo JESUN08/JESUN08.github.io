7ff74a1687a393e8ac449a96183cd357892e8c4a
JESUN08 <jesun.08.official@gmail.com>
2026-05-23 10:46:14 +0600

    Update service worker cache version and assets
    Updated cache version and added new icons to cache.

--- sw.js
+++ sw.js
@@ -1,14 +1,23 @@
-const CACHE_NAME = "jesun-v1";
+const CACHE_NAME = "jesun-v3";
 const urlsToCache = [
   "/",
   "/index.html",
-  "/icons/icon-192.png",
-  "/icons/icon-512.png"
+  "/icons/android-chrome-192x192.png",
+  "/icons/favicon.ico"
 ];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
   );
 });
 
